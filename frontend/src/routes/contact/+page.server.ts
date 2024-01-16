eed5cf665c6bc292eb9d6cdce4673437ef550545
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-01-15 19:06:15 -0800

    updated my code to use server side

---
+++ frontend/src/routes/contact/+page.server.ts
@@ -0,0 +1,108 @@
+import { fail } from '@sveltejs/kit';
+import { isValidEmail } from '$source/lib/utils';
+
+const API_ENDPOINT = 'https://api.web3forms.com';
+
+interface Validation {
+	nameMissing?: string;
+	emailMissing?: string;
+	messageMissing?: string;
+	nameLimit?: string;
+	emailLimit?: string;
+	messageLimit?: string;
+	emailFormat?: string;
+}
+
+export const actions = {
+	default: async ({ request }) => {
+		const startTime = performance.now();
+
+		const data = await request.formData();
+		const { access_key, subject, from_name, name, email, message } = props(data);
+
+		const validate = typedProps({ name, email, message });
+
+		if (Object.keys(validate).length > 0) {
+			return fail(400, {
+				required: !name || !email || !message,
+				errors: validate
+			});
+		}
+
+		const response = await fetch(`${API_ENDPOINT}/submit`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body: JSON.stringify({ access_key, subject, from_name, name, email, message })
+		});
+
+		const endTime = performance.now();
+		const requestTime = endTime - startTime;
+
+		if (requestTime < 10) {
+			const errors: Validation = {
+				messageLimit: 'Message limit has been exceeded to over 10ms'
+			};
+			return fail(400, { errors });
+		}
+
+		const responseData = await response.json();
+		return responseData;
+	}
+};
+
+const typedProps = ({
+	name,
+	email,
+	message
+}: {
+	name: string;
+	email: string;
+	message: string;
+}): Validation => {
+	const errors: Validation = {};
+
+	if (!name) {
+		errors.nameMissing = 'Name is required';
+	}
+	if (!email) {
+		errors.emailMissing = 'Email is required';
+	}
+	if (!message) {
+		errors.messageMissing = 'Message is required';
+	}
+	if (name.length < 8) {
+		errors.nameLimit = 'Name must be at least 8 characters';
+	}
+	if (email.length < 15) {
+		errors.emailLimit = 'Email must be at least 15 characters';
+	}
+	if (message.length < 50) {
+		errors.messageLimit = 'Message must be at least 50 characters';
+	}
+	if (isValidEmail(email)) {
+		errors.emailFormat =
+			'Supported domains as of today are: .com, .edu, .org, .io. Please use a valid email address.';
+	}
+	return errors;
+};
+
+const props = (data: FormData) => {
+	const access_key = 'ba3c1bc3-5ea1-4468-b24f-d712fecd17f7';
+	const subject = `${data.get('name')} sent a message from Personal Website Contact Form`;
+	const from_name = 'NEW MESSAGE';
+	const name = data.get('name') as string;
+	const email = data.get('email') as string;
+	const message = data.get('message') as string;
+
+	return {
+		access_key,
+		subject,
+		from_name,
+		name,
+		email,
+		message
+	};
+};
