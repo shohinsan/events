29d0773225b63e61f9d8892a5810577c412f4fe8
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-01-18 23:23:18 -0800

    add general country information

--- frontend/src/routes/contact/+page.server.ts
+++ frontend/src/routes/contact/+page.server.ts
@@ -12,12 +12,14 @@ import type { Data, Form } from '$customTypes';
 import type { ActionData } from '../$types';
 
 const API_ENDPOINT = EMAIL_SERVICE_API;
-
 export const actions = {
 	default: async ({ request }: { request: Request }) => {
 		const data: FormData = await request.formData();
 		const { access_key, subject, from_name, name, email, message, phone } = contactForm(data);
-		const props = { access_key, subject, from_name, name, email, message, phone };
+
+		const country = request.headers.get('cf-ipcountry') as string | undefined;
+
+		const props = { access_key, subject, from_name, name, email, message, phone, country };
 		const validate = typed({ name, email, message, phone });
 
 		if (Object.keys(validate).length > 0) {
@@ -36,7 +38,16 @@ export const actions = {
 			body: JSON.stringify(props)
 		});
 
-		await telegramBotMiddleware({ name, email, phone, message });
+		await telegramBotMiddleware(
+			{
+				name,
+				email,
+				phone,
+				message
+			},
+			country
+		);
+
 		return await response.json();
 	}
 } satisfies ActionData;
@@ -51,7 +62,10 @@ const contactForm = (data: FormData): Data => ({
 	phone: data.get('phone') ? `+${data.get('phone')}` : undefined
 });
 
-const telegramBotMiddleware = async ({ name, email, phone, message }: Form) => {
+const telegramBotMiddleware = async (
+	{ name, email, phone, message }: Form,
+	country: string | undefined
+) => {
 	const telegramBot = newTelegramBot(TELEGRAM_BOT_TOKEN);
 
 	const textBuilder = `*New form submission from ${name}*
@@ -59,9 +73,10 @@ const telegramBotMiddleware = async ({ name, email, phone, message }: Form) => {
 	| *Contact details*
 	|-------------------------
 	   *Email*   -  ${email}      
-	   *Phone*  -  ${phone}              
+	   *Phone*  -  ${phone} 
+	   *Country*  -  ${country}
 	
-	| *Message*
+    | *Message*
 	|-------------------------
 	   ${message}`;
 
