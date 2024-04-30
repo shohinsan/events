8c1135248a92021271c23ad84317febeb829fa4c
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-04-30 00:27:46 -0700

    chore: fix metadata

--- frontend/src/lib/components/system/Metadata.svelte
+++ frontend/src/lib/components/system/Metadata.svelte
@@ -8,21 +8,7 @@
 		? `${$page.data.title} - ${site.createSite.name}`
 		: site.createSite.name;
 
-
-	 
-
-
-
-
 </script>
-
-<script type="ts">
-	(function(c,l,a,r,i,t,y){
-	    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-	    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-	    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-	    })(window, document, "clarity", "script", "m4h2srehhk");
-</script> 
 
 <svelte:head>
 	<title>{title}</title>
