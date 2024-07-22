2f63f7daf06b21bc232d12a1abaffed5d74c6cc2
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-07-22 01:47:58 -0700

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -11,21 +11,21 @@
 	<title>{site.createSite.name}</title>
 	<meta name="twitter:title" content={site.createSite.name} />
 	<meta name="twitter:description" content={site.createSite.description} />
-	<meta name="Description" content={site.createSite.description} />
+	<meta name="description" content={site.createSite.description} />
 </svelte:head>
 
-<div class={cn('my-16 sm:mb-52 sm:mt-32')}>
+<section class={cn('my-16 sm:mb-52 sm:mt-32')}>
 	<div class="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-		<div class="lg:pl-20">
-			<div class="max-w-xs px-2.5 lg:max-w-none">
+		<aside class="lg:pl-20">
+			<figure class="max-w-xs px-2.5 lg:max-w-none">
 				<img
 					src="https://pub-c1d83372ed0146a9ae11bca340543efb.r2.dev/me.png"
 					alt="Shohin Abdulkhamidov"
 					class="aspect-square rotate-3 rounded-2xl bg-zinc-800 object-cover"
 				/>
-			</div>
-		</div>
-		<div class="lg:order-first lg:row-span-2">
+			</figure>
+		</aside>
+		<article class="lg:order-first lg:row-span-2">
 			<h1 class="text-4xl font-bold tracking-tight sm:text-5xl">
 				I’m Shohin Abdulkhamidov, an inspiring Software Engineer based in California.
 			</h1>
@@ -43,20 +43,17 @@
 					and have some fun.
 				</p>
 
+				<h2 class="text-2xl font-bold">Technologies I've been working with recently:</h2>
 				<p>
-					Here are a few technologies I've been working with recently:
-					<span>SvelteKit, NextJS, Java, and Python.</span>
-					I'm now expanding my skill set even further by
-					<span
-						>learning Golang and eventually making it my main go-to language. Lately,
-						due to the AI hype, I've become curious about PyTorch and AutoGen.</span
-					>
-					Additionally, I keep up with technology news by reading
-					<a
+					SvelteKit, NextJS, Java, and Python. I'm now expanding my skill set even further
+					by learning Golang and eventually making it my main go-to language. Lately, due
+					to the AI hype, I've become curious about PyTorch and AutoGen. Additionally, I
+					keep up with technology news by reading <a
 						class="text-blue-500 hover:text-primary"
 						href="https://refer.tldr.tech/0f5ffe59">tldr.tech newsletters</a
 					>.
 				</p>
+
 				<div class="flex gap-4 select-none">
 					<Cloudflare width={36} height={36} />
 					<PWA width={36} height={36} />
@@ -70,67 +67,68 @@
 					question, or just want to connect.
 				</p>
 			</div>
-		</div>
-		<div class="lg:pl-20">
+		</article>
+
+		<aside class="lg:pl-20">
 			<form method="GET" action="SHOHIN_ABDULKHAMIDOV_RESUME">
 				<Button
 					type="submit"
 					class={cn(`
-            group mb-8 flex w-full items-center justify-center rounded-md px-3 text-center
-        `)}
-				>
-					<span class="mr-5">Download CV</span>
-					<Download /></Button
-				>
-			</form>
-
-			{#each link.professional as { name, href, icon }}
-				<ul
-					role="list"
-					class={cn(`
-						group flex items-center text-sm font-medium transition hover:text-primary
+						group mb-8 flex w-full items-center justify-center rounded-md px-3 text-center
 					`)}
 				>
-					{#if icon}
-						<span class="flex-shrink-0">
-							<svelte:component this={icon} />
-						</span>
-					{/if}
-					<li
-						aria-current={$page.url.pathname === href ? 'page' : undefined}
-						class={cn(`
-							inline-block px-4 py-2
-						`)}
-					>
-						<a {href}>{name}</a>
-					</li>
+					<span class="mr-5">Download CV</span>
+					<Download />
+				</Button>
+			</form>
+
+			<nav aria-label="Professional links">
+				<ul>
+					{#each link.professional as { name, href, icon }}
+						<li>
+							<a
+								class={cn(`
+									group flex items-center text-sm font-medium transition hover:text-primary
+								`)}
+								{href}
+								aria-current={$page.url.pathname === href ? 'page' : undefined}
+							>
+								{#if icon}
+									<span class="flex-shrink-0">
+										<svelte:component this={icon} />
+									</span>
+								{/if}
+								<span class="inline-block px-4 py-2">{name}</span>
+							</a>
+						</li>
+					{/each}
 				</ul>
-			{/each}
+			</nav>
 
 			<div class="mt-8 border-t border-zinc-700/40 pt-8" />
 
-			{#each link.personal as { name, href, icon }}
-				<ul
-					role="list"
-					class={cn(`
-						group flex items-center text-sm font-medium  transition hover:text-primary
-					`)}
-				>
-					{#if icon}
-						<span class="flex-shrink-0">
-							<svelte:component this={icon} />
-						</span>
-					{/if}
-					<li
-						aria-current={$page.url.pathname === href ? 'page' : undefined}
-						class={cn(`
-							inline-block px-4 py-2
-						`)}
-					>
-						<a {href}>{name}</a>
-					</li>
+			<nav aria-label="Personal links">
+				<ul>
+					{#each link.personal as { name, href, icon }}
+						<li>
+							<a
+								class={cn(`
+									group flex items-center text-sm font-medium transition hover:text-primary
+								`)}
+								{href}
+								aria-current={$page.url.pathname === href ? 'page' : undefined}
+							>
+								{#if icon}
+									<span class="flex-shrink-0">
+										<svelte:component this={icon} />
+									</span>
+								{/if}
+								<span class="inline-block px-4 py-2">{name}</span>
+							</a>
+						</li>
+					{/each}
 				</ul>
-			{/each}
-		</div>
+			</nav>
+		</aside>
 	</div>
-</div>
+</section>
