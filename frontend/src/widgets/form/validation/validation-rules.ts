697f262915b87ffb84baf01dd77d75d50cd55c6d
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-07-23 15:32:29 -0700

    Update validation-rules.ts

--- frontend/src/widgets/form/validation/validation-rules.ts
+++ frontend/src/widgets/form/validation/validation-rules.ts
@@ -9,13 +9,26 @@ const phoneRule = z
 	.regex(/^\d{10}$/, 'Phone number must be 10 digits without spaces or dashes')
 	.optional();
 
+const disallowedDomains = [
+	'aol.com',
+	'searchindex.site',
+];
+
 const emailRule = z
 	.string()
 	.email('Invalid email')
 	.regex(
 		/^[a-zA-Z0-9._-]+@(?:[a-zA-Z0-9-]+\.)+(com|edu|org|io)$/,
 		'Email can only contain letters, numbers, underscores, periods, and hyphens'
-	);
+	)
+	.refine((email) => {
+		const domain = email.split('@')[1];
+		const disallowedDomain = disallowedDomains.find(d => d === domain);
+		return !disallowedDomain;
+	}, (email) => {
+		const domain = email.split('@')[1];
+		return { message: `Domain ${domain} is not allowed` };
+	});
 
 const messageRule = z.string();
 
